0346200c5b7b5e026fcc404750a5a8e3004cd7a4
Dario Villamizar <darvilla99@gmail.com>
2024-06-02 21:38:10 -0500

    Actualizacion ejercicios Node.js

--- NodeJS/Myfirst.js
+++ NodeJS/Myfirst.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./Myfirstmodule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello world!');
+    res.end('Hello world! my date and time is: ' + dt.myDateTime());
+    res.end(); 
 }).listen(8080);
